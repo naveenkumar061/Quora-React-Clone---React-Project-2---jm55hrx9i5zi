b610faa3c3b906c506b458eba7003164bb3aed99
naveenkumar061 <devarashettynaveen224@gmail.com>
2024-07-25 18:46:35 +0530

    Final Commit 5 Before Review

--- src/components/utils/UnderMaintenance.js
+++ src/components/utils/UnderMaintenance.js
@@ -2,7 +2,7 @@ import maintainence from '../assets/images/maintenance.svg';
 
 function UnderMaintenance({ children }) {
   return (
-    <div className="min-h-screen w-full pb-4">
+    <div className="min-h-screen w-full pb-4 bg-white dark:bg-[#181818] dark:text-[#b1b3b6]">
       <div className="w-full mt-7 md:w-9/12 px-3 mx-auto flex flex-col justify-center items-center gap-4 md:pt-4">
         <div className="text-center text-lg">
           This part of website is under maintainence
